a6b85e894d9f6a65612835ed50b30be4752e0863
PRITAM MONDAL <131168290+RESK3N@users.noreply.github.com>
2024-11-13 23:03:24 +0530

    update c++

--- P-22.cpp
+++ P-22.cpp
@@ -1,8 +1,28 @@
 //Write a Program in C++ to overload binary "+" operator
 #include<iostream>
 using namespace std;
-class over{
-    struct complex{
-        float 
-    };
+class complex {
+    int img, rl;
+public:
+    complex(int r=0, int i=0){
+        img=i;
+        rl=r;
+    }
+
+    complex operator+ (complex &obj) {
+        //cout<<rl<<endl<<img<<endl;
+        complex res;
+        res.rl = rl + obj.rl;
+        res.img = img + obj.img;
+        return res;
+    }
+    void display(){
+        cout<<"Sum is "<<rl<<" + i "<<img;
+    }
 };
+int main(){
+    complex a(5,6),b(10,4);
+    complex c=a+b;
+    c.display();
+
+}
